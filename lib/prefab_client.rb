4f0996ff296a0e617c5f7515e0a2e3cebe751f45
Jeff Dwyer <jdwyah@gmail.com>
2017-12-16 22:05:36 -0500

    require tweaks

--- lib/prefab_client.rb
+++ lib/prefab_client.rb
@@ -4,32 +4,15 @@ require 'bundler'
 Bundler.require(:default, :development)
 require "concurrent/atomics"
 require 'concurrent'
-require_relative 'prefab/prefab_pb'
-require_relative 'prefab/prefab_services_pb'
-require_relative 'prefab/ratelimit_pb'
-require_relative 'prefab/config_loader'
-require_relative 'prefab/config_resolver'
-require_relative 'prefab/client'
-require_relative 'prefab/ratelimit_client'
-require_relative 'prefab/config_client'
-require_relative 'prefab/auth_interceptor'
-require_relative 'prefab/noop_cache'
-require_relative 'prefab/noop_stats'
+require 'prefab_pb'
+require 'prefab_services_pb'
+require 'ratelimit_pb'
+require 'prefab/config_loader'
+require 'prefab/config_resolver'
+require 'prefab/client'
+require 'prefab/ratelimit_client'
+require 'prefab/config_client'
+require 'prefab/auth_interceptor'
+require 'prefab/noop_cache'
+require 'prefab/noop_stats'
 
-
-client = Prefab::Client.new(api_key: ENV["RATELIMIT_API_KEY"], local: false)
-
-start = Time.now
-puts "pass? #{client.ratelimit_client.pass? "hubtest.secondly"}"
-puts "pass? #{client.ratelimit_client.pass? "hubtest.secondly"}"
-puts "pass? #{client.ratelimit_client.pass? "hubtest.secondly"}"
-puts "pass? #{client.ratelimit_client.pass? "hubtest.secondly"}"
-puts "pass? #{client.ratelimit_client.pass? "hubtest.secondly"}"
-puts "pass? #{client.ratelimit_client.pass? "hubtest.secondly"}"
-puts "pass? #{client.ratelimit_client.pass? "hubtest.secondly"}"
-puts "pass? #{client.ratelimit_client.pass? "hubtest.secondly"}"
-
-puts "took #{Time.now - start}"
-
-# @@prefab = Prefab::Client.new(api_key: ENV["RATELIMIT_API_KEY"], local: true)
-# @@prefab.rate_limits.check()
