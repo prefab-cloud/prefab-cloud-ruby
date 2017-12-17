e9886c318cccacb5c2a167db008ae96ad39765bd
Jeff Dwyer <jdwyah@gmail.com>
2017-12-16 21:43:13 -0500

    cleanup

--- generate.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-grpc_tools_ruby_protoc -I /Users/jeffdwyer/Documents/workspace/RateLimitInc/ratelimit-java/src/main/proto --ruby_out=lib/prefab --grpc_out=lib/prefab ratelimit.proto prefab.proto
