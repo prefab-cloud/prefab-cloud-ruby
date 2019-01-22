ff1c3d0fc682841a93b71697dad5654c49733c13
Jeff Dwyer <jdwyah@gmail.com>
2019-01-22 13:45:41 -0500

    add client version to grpc headers

--- lib/prefab/auth_interceptor.rb
+++ lib/prefab/auth_interceptor.rb
@@ -1,25 +1,29 @@
 class AuthInterceptor < GRPC::ClientInterceptor
   def initialize(api_key)
+    version = File.exist?('VERSION') ? File.read('VERSION').chomp : ""
+    @client = "prefab-cloud-ruby.#{version}".freeze
     @api_key = api_key
   end
 
-  def request_response(request:, call:, method:, metadata:)
-    metadata['auth'] = @api_key
-    yield
+  def request_response(request:, call:, method:, metadata:, &block)
+    shared(metadata, &block)
   end
 
-  def client_streamer(requests:, call:, method:, metadata:)
-    metadata['auth'] = @api_key
-    yield
+  def client_streamer(requests:, call:, method:, metadata:, &block)
+    shared(metadata, &block)
   end
 
-  def server_streamer(request:, call:, method:, metadata:)
-    metadata['auth'] = @api_key
-    yield
+  def server_streamer(request:, call:, method:, metadata:, &block)
+    shared(metadata, &block)
   end
 
-  def bidi_streamer(requests:, call:, method:, metadata:)
+  def bidi_streamer(requests:, call:, method:, metadata:, &block)
+    shared(metadata, &block)
+  end
+
+  def shared(metadata)
     metadata['auth'] = @api_key
+    metadata['client'] = @client
     yield
   end
 end
