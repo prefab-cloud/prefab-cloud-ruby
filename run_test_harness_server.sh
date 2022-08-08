724edb91e2e18c8620e751ba5182e6c4f0f8358a
Jeffrey Chupp <jeff@semanticart.com>
2022-08-08 08:37:21 -0400

    Fixes for changes to harness server (#34)
    This updates to play nicely with https://github.com/prefab-cloud/prefab-cloud-client-tester/pull/3

--- run_test_harness_server.sh
+++ run_test_harness_server.sh
@@ -1,2 +1,8 @@
-PREFAB_LOG_CLIENT_BOOTSTRAP_LOG_LEVEL=debug PREFAB_CLOUD_HTTP=true PREFAB_GRPC_URL="localhost:50051" ruby -Ilib test/harness_server.rb
-#GRPC_TRACE=all GRPC_VERBOSITY=DEBUG PREFAB_LOG_CLIENT_BOOTSTRAP_LOG_LEVEL=debug PREFAB_CLOUD_HTTP=true PREFAB_API_KEY="1|local_development_api_key" PREFAB_API_URL="localhost:50051" ruby -Ilib test/harness_server.rb
+#! /usr/bin/env bash
+
+PREFAB_CDN_URL="https://api-prefab-cloud.global.ssl.fastly.net" \
+  PREFAB_LOG_CLIENT_BOOTSTRAP_LOG_LEVEL=debug \
+  PREFAB_CLOUD_HTTP=true \
+  PREFAB_API_KEY="1|local_development_api_key" \
+  PREFAB_GRPC_URL="localhost:50051" \
+  ruby -Ilib test/harness_server.rb
