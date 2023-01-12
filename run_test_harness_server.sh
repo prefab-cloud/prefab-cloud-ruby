c29076e6e411867fd391307cfd3d0cfeafb0647e
Michael Berkowitz <michael.berkowitz@gmail.com>
2023-01-12 17:17:02 -0500

    Remove code for the old test harness

--- run_test_harness_server.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-
-PREFAB_CDN_URL="https://api-prefab-cloud.global.ssl.fastly.net" \
-  PREFAB_LOG_CLIENT_BOOTSTRAP_LOG_LEVEL=debug \
-  PREFAB_CLOUD_HTTP=true \
-  PREFAB_API_KEY="1|local_development_api_key" \
-  PREFAB_GRPC_URL="localhost:50051" \
-  ruby -Ilib test/harness_server.rb
