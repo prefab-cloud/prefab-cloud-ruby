d0f3eaf8e0dd63dd63383cc766a8f41e3a3130b2
Jeff Dwyer <jdwyah@gmail.com>
2023-05-30 22:21:00 -0400

    rbs wip

--- lib/prefab/prefab_config.rb
+++ lib/prefab/prefab_config.rb
@@ -1,23 +1,41 @@
 module Prefab
-  class Feature123
-
-    TREATMENT = 'treatment'
-    CONTROL = 'control'
-
-    def value
-      CONTROL
+  module Feature123
+    class Treatment
+    end
+    class Control
+    end
+    def value_of(value)
+      Prefab::Feature123::Control.new if value == CONTROL
+      Prefab::Feature123::Treatment.new if value == TREATMENT
     end
   end
   class Config
     class << self
       def feature_123
-        config_client.get("feature_123", false)
+        value_of(config_client.get("feature_123"))
       end
-
+      def feature_123_is_treatment?
+        feature_123 == Prefab::Feature123::TREATMENT
+      end
+      def feature_123_is_control?
+        feature_123 == Prefab::Feature123::Control
+      end
+      def feature_bool?
+        config_client.get_bool("feature_bool")
+      end
+      def http_timeout
+        config_client.get_int("http_timeout")
+      end
+      def http_retries
+        config_client.get_int("http_retries")
+      end
+      def db_host
+        config_client.get_string("db_host")
+      end
     end
   end
 end
 
-f = PrefabConfig.feature_123
+f = PrefabConfig.feature_123 == Prefab::Feature123::TREATMENT
 
-PrefabConfig
+PrefabConfig.class
