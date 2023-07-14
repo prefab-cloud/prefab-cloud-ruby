f6c2ae6c713123b4d82b3f70461d1f80447d3936
Jeffrey Chupp <jeff@semanticart.com>
2023-07-14 16:40:25 -0400

    Pass selected_value to the proto

--- test/test_evaluation_summary_aggregator.rb
+++ test/test_evaluation_summary_aggregator.rb
@@ -6,12 +6,16 @@ require 'timecop'
 class TestEvaluationSummaryAggregator < Minitest::Test
   EFFECTIVELY_NEVER = 99_999 # we sync manually
 
+  EXAMPLE_VALUE_1 = PrefabProto::ConfigValue.new(bool: true)
+  EXAMPLE_VALUE_2 = PrefabProto::ConfigValue.new(bool: false)
+
   EXAMPLE_COUNTER = {
     config_id: 1,
     selected_index: 2,
     config_row_index: 3,
     conditional_value_index: 4,
-    weighted_value_index: 5
+    weighted_value_index: 5,
+    seleced_value: EXAMPLE_VALUE_1
   }.freeze
 
   def test_increments_counts
@@ -39,13 +43,13 @@ class TestEvaluationSummaryAggregator < Minitest::Test
     expected = {
       ['config-1', :CONFIG] => {
         { config_id: 1, selected_index: 2, config_row_index: 3, conditional_value_index: 4,
-          weighted_value_index: 5 } => 3,
+          weighted_value_index: 5, selected_value: EXAMPLE_VALUE_1 } => 3,
         { config_id: 1, selected_index: 3, config_row_index: 7, conditional_value_index: 8,
-          weighted_value_index: 10 } => 1
+          weighted_value_index: 10, selected_value: EXAMPLE_VALUE_2 } => 1
       },
       ['config-2', :FEATURE_FLAG] => {
         { config_id: 2, selected_index: 3, config_row_index: 5, conditional_value_index: 7,
-          weighted_value_index: 6 } => 9
+          weighted_value_index: 6, selected_value: EXAMPLE_VALUE_1 } => 9
       }
     }
 
@@ -90,6 +94,7 @@ class TestEvaluationSummaryAggregator < Minitest::Test
                 config_row_index: 3,
                 conditional_value_index: 4,
                 weighted_value_index: 5,
+                selected_value: EXAMPLE_VALUE_1,
                 count: 3
               ),
               PrefabProto::ConfigEvaluationCounter.new(
@@ -98,6 +103,7 @@ class TestEvaluationSummaryAggregator < Minitest::Test
                 config_row_index: 7,
                 conditional_value_index: 8,
                 weighted_value_index: 10,
+                selected_value: EXAMPLE_VALUE_2,
                 count: 1
               )
             ]
@@ -112,6 +118,7 @@ class TestEvaluationSummaryAggregator < Minitest::Test
                 config_row_index: 5,
                 conditional_value_index: 7,
                 weighted_value_index: 6,
+                selected_value: EXAMPLE_VALUE_1,
                 count: 9
               )
             ]
@@ -140,13 +147,13 @@ class TestEvaluationSummaryAggregator < Minitest::Test
     data = {
       ['config-1', :CONFIG] => {
         { config_id: 1, selected_index: 2, config_row_index: 3, conditional_value_index: 4,
-          weighted_value_index: 5 } => 3,
+          weighted_value_index: 5, selected_value: EXAMPLE_VALUE_1 } => 3,
         { config_id: 1, selected_index: 3, config_row_index: 7, conditional_value_index: 8,
-          weighted_value_index: 10 } => 1
+          weighted_value_index: 10, selected_value: EXAMPLE_VALUE_2 } => 1
       },
       ['config-2', :FEATURE_FLAG] => {
         { config_id: 2, selected_index: 3, config_row_index: 5, conditional_value_index: 7,
-          weighted_value_index: 6 } => 9
+          weighted_value_index: 6, selected_value: EXAMPLE_VALUE_1 } => 9
       }
     }
 
